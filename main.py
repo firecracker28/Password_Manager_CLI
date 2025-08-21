1a91ec5936fc5ff281e5136d1a7d287167c931ab
firecracker28 <loganoscarschmidt@gmail.com>
2025-08-21 12:03:13 -0500

    cleaned up argparser

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from cli import add,delete,modify,search,view,generate
  #TODO create arg parser
 parser = argparse.ArgumentParser(prog ='main',allow_abbrev=False)
 # Allows user only one action per session
-parser.add_argument('--a',required=True)
+parser.add_argument('--action',required=True)
 parser.add_argument('--service')
 parser.add_argument('--password')
 parser.add_argument('--username')
@@ -21,9 +21,6 @@ parser.add_argument('--notes')
 
 
 args = parser.parse_args()
-if args.a == 'help':
-    print('Usage: python3 main.py -a(action:add,delete,modify,search,view,generate,help) Optional: --service, --password, --username,--notes')
-    sys.exit()
 # checks prerequsites
 try:
     with open('config/auth.json','r') as file:
@@ -47,17 +44,17 @@ else:
         sys.exit()
     manager = cp.read_encrypted(path ='vault/vault.crypt',password=master)
 
-if args.a == 'add':
+if args.action == 'add':
     manager = add(manager,args.service,args.password,args.username,args.notes)
-elif args.a == 'delete':
+elif args.action == 'delete':
     manager = delete(manager,args.service)
-elif args.a == 'modify':
+elif args.action == 'modify':
     manager = modify(manager,args.service,args.password,args.username,args.notes)
-elif args.a == 'search':
+elif args.action == 'search':
     search(manager,args.service)
-elif args.a == 'view':
+elif args.action == 'view':
     view(manager)
-elif args.a == 'generate':
+elif args.action == 'generate':
     generate()
 else:
     print("Action given is not a valid action. Please try again")
